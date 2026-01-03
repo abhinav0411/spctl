7ece993f71fe193c8dcba3c33d198a415e31d33d
abhinav0411 <abhinavrai0419@gmail.com>
2026-01-03 23:58:36 +0530

    Made init model

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+const (
+	username
+	password
+)
+
+type login struct {
+	inputs []textinput.Model
+	err    error
+}
 
 func main() {
-	fmt.Println("Hello world")
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initialModel() login {
+	var inputs[username] = textinput.New()
+	inputs[username].Focus()
+
 }
