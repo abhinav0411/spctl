be631581bcece52baa7f75015ed6a61f3873eadf
abhinav0411 <abhinavrai0419@gmail.com>
2026-02-16 23:47:17 +0530

    changed the structure of th project

---
+++ cmd/spctl/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// m := ui.NewScreen()
+	// p := tea.NewProgram(m, tea.WithAltScreen())
+	// _, err := p.Run()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	fmt.Printf("hello world")
+}
