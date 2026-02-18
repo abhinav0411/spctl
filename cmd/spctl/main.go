c75775c9b9182ee1038cfbde03526e7357834058
abhinav0411 <abhinavrai0419@gmail.com>
2026-02-19 02:13:55 +0530

    Done with code verifier and getting code from spotify

--- cmd/spctl/main.go
+++ cmd/spctl/main.go
@@ -4,14 +4,9 @@ import (
 	"fmt"
 
 	"github.com/abhinav0411/spctl/spotify"
-	"golang.org/x/oauth2"
 )
 
 func main() {
-	var verifier = spotify.GenerateRandomString()
-	var conf = spotify.CreateConf()
-
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(verifier))
-
-	fmt.Printf("visit this url \n%v\n", url)
+	spotify.Login()
+	fmt.Println("Hope this works")
 }
